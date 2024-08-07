7a45af6768ee663b62febacf8228d8a9702eabad
Maksim Sukhov <silveraven@yandex.ru>
2024-08-07 13:28:53 +0200

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -38,8 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let newObj = {};
+    let str = "";
+  
+    for (const key in MORSE_TABLE) {
+      let newKeys = key.split("").map(item => {
+        if(item === ".") {
+          return "10";
+        } else {
+          return "11";
+        }
+      });
+      
+      let keysLengthTen = newKeys.join("").padStart(10, "0"); 
+      newObj[keysLengthTen] = MORSE_TABLE[key]
+    }
+  
+    for (let index = 0; index < expr.length; index+=10) {
+      let tenPer = expr.slice(index, index + 10);
+      if(newObj[tenPer]) {
+        str+=newObj[tenPer];
+      }else if(tenPer === "**********") {
+        str+=" ";
+      }
+    }
+  
+    return str;
+  }
 
 module.exports = {
     decode
